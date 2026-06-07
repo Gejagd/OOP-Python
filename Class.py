8699cf248437d9564e1a47ca1fe8cd398ed6d7d4
Gejagd <gejagd.dev@gmail.com>
2026-06-07 15:05:07 +0700

    Adding new class

--- Class.py
+++ Class.py
@@ -6,7 +6,18 @@ class Mahasiswa:
     def __str__(self):
         return(f"Nama: {self.nama}, NIM: {self.nim}")
 
+class Atk:
+    def __init__(self, alat, jumlah):
+        self.alat = alat
+        self.jumlah = jumlah
+        
+    def __str__(self):
+        return(f"Nama Alat: {self.alat}, Jumlah: {self.jumlah}")
+
 mhs1 = Mahasiswa("Andi", "123")
+atk1 = Atk("Pulpen", "1")
+
 print(mhs1)
+print(atk1)
 # print(mhs1.nama)
 # print(mhs1.nim)
